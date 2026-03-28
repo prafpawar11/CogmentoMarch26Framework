c45db1d090fd6b27947babce97cd3f08f6ecc32a
mayur-jadhav07 <jmayur309@gmail.com>
2026-03-28 20:40:24 +0530

    Added Deals page Test cases

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,6 +21,8 @@ public class BaseTest {
 
 		driver.get("https://ui.cogmento.com/");
 
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
 		driver.findElement(By.name("email")).sendKeys("prafulp1010@gmail.com");
 
 		driver.findElement(By.name("password")).sendKeys("Pr@ful0812");
@@ -32,27 +36,52 @@ public class BaseTest {
 	public void validateLoginFunctionality() {
 		Assert.assertEquals(driver.getTitle(), "Free CRM");
 	}
-	
+
 	@Test(priority = 2)
 	public void createContact() throws InterruptedException {
-		
+
 		driver.findElement(By.xpath("//span[text()='Contacts']")).click();
-		
+
 		driver.findElement(By.xpath("//button[text()='Create']")).click();
-		
-		Thread.sleep(3000);
-		
+
+		Thread.sleep(2000);
+
 		driver.findElement(By.name("first_name")).sendKeys("Abhijeet");
-		
+
 		driver.findElement(By.name("last_name")).sendKeys("Kanade");
-		
+
 		driver.findElement(By.name("description")).sendKeys("TC-003");
-		Thread.sleep(3000);
-		
+		Thread.sleep(2000);
+
 		driver.findElement(By.xpath("//button[text()='Save']")).click();
+
+		Thread.sleep(3000);
 	}
 
-	
+	@Test(priority = 3)
+	public void createDeal() throws InterruptedException {
+
+		driver.findElement(By.xpath("//a[@href='/deals']")).click();
+
+		driver.findElement(By.xpath("//a[@href='/deals/new']")).click();
+
+		Thread.sleep(2000);
+
+		driver.findElement(By.name("title")).sendKeys("Motorola Edge 50");
+
+		driver.findElement(By.name("description")).sendKeys("8GB RAM 128GB ROM");
+
+		driver.findElement(By.name("identifier")).sendKeys("Edge 50");
+
+		driver.findElement(By.xpath("//button[text()='Save']")).click();
+
+		boolean isDisplayed = driver.findElement(By.xpath("//span[@class=\"selectable \"]")).isDisplayed();
+
+		Thread.sleep(2000);
+
+		Assert.assertTrue(isDisplayed);
+	}
+
 	@AfterTest
 	public void teardown() throws InterruptedException {
 
